7de070adc6da49424fa424441c0c3e1b25450ac3
Thomas Rudin <thomas@rudin.io>
2018-08-23 14:31:24 +0200

    restrict y space

--- init.lua
+++ init.lua
@@ -67,3 +67,26 @@ minetest.register_alias("rangedweapons:10mm", "default:cobble")
 minetest.register_alias("rangedweapons:375", "default:cobble")
 minetest.register_alias("rangedweapons:lasershot", "default:cobble")
 
+
+-- 2018-08-23 y limit (reserved space)
+local timer = 0
+minetest.register_globalstep(function(dtime)
+   timer = timer + dtime
+   
+   -- every 2 seconds
+   if timer < 2 then return end
+   timer = 0
+   
+   for _,player in ipairs(minetest.get_connected_players()) do
+
+   local pos = player:getpos()
+
+   if pos.y > 10000 then
+      player:setpos({x=pos.x, y=9950, z=pos.z})
+      minetest.chat_send_player(player:get_player_name(), "The area above 10k is reserved for future use, sorry...")
+   end
+
+   end
+end)
+
+
