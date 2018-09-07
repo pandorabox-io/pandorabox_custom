e814fa6c51e8f219cab1bfb0924c58f23bcec7e9
Thomas Rudin <thomas@rudin.io>
2018-09-07 12:02:27 +0200

    colored wool works

--- init.lua
+++ init.lua
@@ -90,3 +90,34 @@ minetest.register_globalstep(function(dtime)
 end)
 
 
+-- 2018-09-07 dye fix
+-- https://github.com/h-v-smacker/technic/blob/a5cfbfdb4989786aacdb24002c9c2bbeb97d028b/technic/machines/register/recipe_defusing.lua
+
+local dyes = {
+	{"violet",     "Violet",     "excolor_violet"},
+	{"brown",      "Brown",      "unicolor_dark_orange"},
+	{"pink",       "Pink",       "unicolor_light_red"},
+	{"dark_grey",  "Dark Grey",  "unicolor_darkgrey"},
+	{"dark_green", "Dark Green", "unicolor_dark_green"},
+}
+
+for i = 1, #dyes do
+	local name, desc, craft_color_group = unpack(dyes[i])
+	minetest.register_craft{
+		type = "shapeless",
+		output = "wool:" .. name,
+		recipe = {"group:dye," .. craft_color_group, "group:wool"},
+	}
+end
+
+
+-- 2018-09-07
+-- bronze block to ingots
+minetest.register_craft({
+	type = "shapeless",
+	output = 'default:bronze_ingot 9',
+	recipe = {
+		'default:bronzeblock'
+	}
+})
+
