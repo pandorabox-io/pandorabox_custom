73e347df1d9d75720dfe4eb31e032bb0cae6af86
Thomas Rudin <thomas@rudin.io>
2018-09-18 13:10:02 +0200

    make cotton grindable

--- init.lua
+++ init.lua
@@ -121,3 +121,11 @@ minetest.register_craft({
 	}
 })
 
+-- 2018-09-18
+-- cotton seed to flour
+
+technic.register_grinder_recipe({
+	input={"farming:seed_cotton"},
+	output="farming:flour 1"
+})
+
