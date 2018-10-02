e0ac65b1c8f48e2233c2609a5830a8fe9f2cb38a
Thomas Rudin <thomas@rudin.io>
2018-10-02 16:35:29 +0200

    priv

--- players.lua
+++ players.lua
@@ -1,4 +1,9 @@
 
+minetest.register_privilege("server_softfull_join", {
+	description = "Can join the server if soft-limit is reached",
+	give_to_singleplayer= false,
+})
+
 minetest.register_on_prejoinplayer(function(name, ip)
 
 	--local privs = minetest.get_player_privs(name)
