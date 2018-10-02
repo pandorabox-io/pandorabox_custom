2d954c916f6285ce50a73e0a915fe39af878a333
Thomas Rudin <thomas@rudin.io>
2018-10-02 16:11:55 +0200

    overflow protection wip

---
+++ players.lua
@@ -0,0 +1,7 @@
+
+minetest.register_on_prejoinplayer(function(name, ip)
+
+	--local privs = minetest.get_player_privs(name)
+	--return minetest.serialize(privs)
+	--TODO: reject new players if server full
+end)
