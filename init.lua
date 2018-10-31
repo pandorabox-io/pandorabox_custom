3c1ccf56f11e1790c7b153ebf9854a71e78ba983
Thomas Rudin <thomas@rudin.io>
2018-10-31 15:45:49 +0100

    cnc recipe

--- init.lua
+++ init.lua
@@ -140,3 +140,23 @@ technic.register_grinder_recipe({
 	output="farming:flour 1"
 })
 
+-- 2018-10-31
+-- cnc nodes
+-- https://github.com/pandorabox-io/pandorabox.io/issues/20
+
+technic.cnc.register_all("scifi_nodes:white2",
+	{cracky=1, not_in_creative_inventory=1},
+	{"scifi_nodes_white2.png"},
+	"White wall"
+)
+
+technic.cnc.register_all("scifi_nodes:black",
+        {cracky=1, not_in_creative_inventory=1},
+        {"scifi_nodes_black.png"},
+        "Black wall"
+)
+
+
+
+
+
