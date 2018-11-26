8155d1dc3579dd03cb469f262bdd9d12f65a594c
Thomas Rudin <thomas@rudin.io>
2018-11-26 10:56:19 +0100

    cnc fix

--- init.lua
+++ init.lua
@@ -144,13 +144,13 @@ technic.register_grinder_recipe({
 -- cnc nodes
 -- https://github.com/pandorabox-io/pandorabox.io/issues/20
 
-technic.cnc.register_all("scifi_nodes:white2",
+technic_cnc.register_all("scifi_nodes:white2",
 	{cracky=1, not_in_creative_inventory=1},
 	{"scifi_nodes_white2.png"},
 	"White wall"
 )
 
-technic.cnc.register_all("scifi_nodes:black",
+technic_cnc.register_all("scifi_nodes:black",
         {cracky=1, not_in_creative_inventory=1},
         {"scifi_nodes_black.png"},
         "Black wall"
