bd50ffdf41b1e2f3a07576a285842f928530fe1a
Thomas Rudin <thomas@rudin.io>
2018-12-31 14:35:13 +0100

    dump some item/node counts

--- init.lua
+++ init.lua
@@ -5,6 +5,18 @@ pandorabox = {
 dofile(MP.."/onplace_restriction.lua")
 dofile(MP.."/loot.lua")
 
+-- https://stackoverflow.com/questions/2705793/how-to-get-number-of-entries-in-a-lua-table
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
+minetest.after(5, function()
+	print("Registered nodes: " .. tablelength(minetest.registered_nodes))
+	print("Registered items: " .. tablelength(minetest.registered_items))
+	print("Registered entities: " .. tablelength(minetest.registered_entities))
+end)
 
 -- 2018-06-20 removed smartrent mod
 -- active again: minetest.register_alias("smartrenting:panel", "air")
