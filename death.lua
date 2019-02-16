158524f3f7a81036e7d7301516e6ad87ec4fb136
Thomas Rudin <thomas@rudin.io>
2019-02-16 13:50:55 +0100

    fix typo

--- death.lua
+++ death.lua
@@ -4,6 +4,6 @@ minetest.register_on_dieplayer(function(player)
 	local player_name = player:get_player_name()
 	local pos = player:get_pos()
 
-	minetest.log("action", "[death] player '" .. player_name .. "' diesd at " .. minetest.pos_to_string(pos))
+	minetest.log("action", "[death] player '" .. player_name .. "' died at " .. minetest.pos_to_string(pos))
 	minetest.chat_send_player(player_name, "You died at " .. minetest.pos_to_string(pos))
 end)
