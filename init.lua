7d3d58fcff7298f4ba4aa510b649ae083f687138
NatureFreshMilk <naturefreshmilk@github.com>
2019-02-25 07:13:58 +0100

    fix ctor/piston attack vector

--- init.lua
+++ init.lua
@@ -195,4 +195,12 @@ minetest.register_alias("technic:cottonseed_oil", "default:cobble")
 minetest.register_alias("chains:chandelier", "homedecor:chandelier_brass")
 minetest.register_alias("basic_materialsgold_wire", "basic_materials:gold_wire")
 
+-- 2019-02-25 fix protector remove vector with piston and constructor
+mesecon.register_mvps_stopper("technic:constructor_mk1_off")
+mesecon.register_mvps_stopper("technic:constructor_mk1_on")
+mesecon.register_mvps_stopper("technic:constructor_mk2_off")
+mesecon.register_mvps_stopper("technic:constructor_mk2_on")
+mesecon.register_mvps_stopper("technic:constructor_mk3_off")
+mesecon.register_mvps_stopper("technic:constructor_mk3_on")
+
 
