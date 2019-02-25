8a7d3e5bf42321e96de8218e36b343baf858aa5a
Thomas Rudin <thomas@rudin.io>
2019-02-25 19:39:24 +0100

    protect_rails priv

--- privs.lua
+++ privs.lua
@@ -4,6 +4,10 @@ minetest.register_privilege("protect_streets", {
 	description = "Protection for streets"
 })
 
+minetest.register_privilege("protect_rails", {
+	description = "Protection for rails/trainlines"
+})
+
 minetest.register_privilege("protect_public_places", {
 	description = "Protection for public places"
 })
