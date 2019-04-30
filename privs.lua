8eae7a5667a6a0cc5b6c8bf55af42d4660c74747
NatureFreshMilk <naturefreshmilk@github.com>
2019-04-30 09:55:29 +0200

    moar privs for priv_protector

--- privs.lua
+++ privs.lua
@@ -1,18 +1,31 @@
 
+-- common/public
 
 minetest.register_privilege("protect_streets", {
-	description = "Protection for streets"
+	description = "Protection for common streets"
 })
 
 minetest.register_privilege("protect_rails", {
-	description = "Protection for rails/trainlines"
+	description = "Protection for common rails/trainlines"
 })
 
 minetest.register_privilege("protect_public_places", {
-	description = "Protection for public places"
+	description = "Protection for common public places"
 })
+
+minetest.register_privilege("protect_public_infrastructure", {
+        description = "Protection for common public infrastructure (machines, etc)"
+})
+
+-- travel
 
 minetest.register_privilege("admin_travel", {
         description = "can use the admins travelnets"
+})
+
+-- specific privs
+
+minetest.register_privilege("protect_technic_city_infra", {
+        description = "Protection for technic city infrastructure"
 })
 
