150fa7d11a7e9643535194baf26cb93add34d03a
NatureFreshMilk <naturefreshmilk@github.com>
2019-05-22 15:07:23 +0200

    userlimit

---
+++ userlimit.lua
@@ -0,0 +1,8 @@
+
+minetest.register_privilege("softlimit_bypass", {
+	description = "can bypass the max player limit"
+})
+
+minetest.register_can_bypass_userlimit(function(name, ip)
+	return minetest.check_player_privs(name, {softlimit_bypass=true})
+end)
