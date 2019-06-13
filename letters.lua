3832fb62a3b772aa0b4e13af24da59f9418483ac
Thomas Rudin <thomas@rudin.io>
2019-06-13 13:37:11 +0200

    scifi letters

---
+++ letters.lua
@@ -0,0 +1,7 @@
+
+letters.register_letters("scifi_nodes", "white", "scifi_nodes:white", "White tile", "scifi_nodes_white.png")
+--letters.register_letters("scifi_nodes", "white2", "scifi_nodes:white2", "White tile2", "scifi_nodes_white2.png")
+--letters.register_letters("scifi_nodes", "blacktile2", "scifi_nodes:blacktile2", "Black tile2", "scifi_nodes_blacktile2.png")
+
+
+
