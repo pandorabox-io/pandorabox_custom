40748dd9741f334a0dbca631218151c7ad105691
NatureFreshMilk <naturefreshmilk@github.com>
2019-06-21 09:00:56 +0200

    cleanup admin travel stuff, use "(P) name" instead

--- privs.lua
+++ privs.lua
@@ -17,12 +17,6 @@ minetest.register_privilege("protect_public_infrastructure", {
         description = "Protection for common public infrastructure (machines, etc)"
 })
 
--- travel
-
-minetest.register_privilege("admin_travel", {
-        description = "can use the admins travelnets"
-})
-
 -- specific privs
 
 minetest.register_privilege("protect_technic_city_infra", {
--- travelnet.lua
+++ travelnet.lua
@@ -20,12 +20,5 @@ travelnet.allow_travel = function( player_name, owner_name, network_name, statio
 		end
 	end
 
-	if owner_name == "admin" then
-		if minetest.check_player_privs(player_name, { admin_travel=true }) then
-			return true
-		else
-			return false
-		end
-	end
 	return true
 end
