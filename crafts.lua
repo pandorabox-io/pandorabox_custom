17f779059065d522ea3737f35503b2d37c12d9c6
Thomas Rudin <thomas@rudin.io>
2019-08-26 17:03:50 +0200

    fix sand recipes

--- crafts.lua
+++ crafts.lua
@@ -55,7 +55,7 @@ local sands = {
 for i = 1, #sands do
 	local output_sand, dye = unpack(sands[i])
 	for j = 1, #sands do
-		local input_sand, _ = unpack(sands[i])
+		local input_sand, _ = unpack(sands[j])
 		if input_sand ~= output_sand then
 			minetest.register_craft({
 				output = output_sand .. " 8",
@@ -79,7 +79,7 @@ local sandstones = {
 for i = 1, #sandstones do
 	local output_sandstone, dye = unpack(sandstones[i])
 	for j = 1, #sandstones do
-		local input_sandstone, _ = unpack(sandstones[i])
+		local input_sandstone, _ = unpack(sandstones[j])
 		if input_sandstone ~= output_sandstone then
 			minetest.register_craft({
 				output = output_sandstone .. " 4",
