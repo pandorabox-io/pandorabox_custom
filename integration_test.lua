5b22d2b03d8dffd36d6316f774895dc0754fa146
NatureFreshMilk <naturefreshmilk@github.com>
2019-09-09 14:50:45 +0200

    more logging for tests

--- integration_test.lua
+++ integration_test.lua
@@ -2,7 +2,11 @@
 minetest.log("warning", "[pandorabox_custom] integration-test enabled!")
 
 minetest.register_on_mods_loaded(function()
+	minetest.log("warning", "[pandorabox_custom] all mods loaded, starting delayed test-function")
+
 	minetest.after(1, function()
+		minetest.log("warning", "[pandorabox_custom] starting integration test")
+
 		local data = minetest.write_json({ success = true }, true);
 		local path = minetest.get_worldpath().."/integration_test.json";
 
@@ -12,6 +16,7 @@ minetest.register_on_mods_loaded(function()
 			file:close();
 		end
 
+		minetest.log("warning", "[pandorabox_custom] integration tests done!")
 		minetest.request_shutdown("success")
 	end)
 end)
