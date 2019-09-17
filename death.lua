b1e3d3c90254f2f66c91c69e8bea1d069e58bed5
NatureFreshMilk <naturefreshmilk@github.com>
2019-09-17 13:57:41 +0200

    happy luacheck

--- death.lua
+++ death.lua
@@ -3,7 +3,7 @@
 minetest.register_on_dieplayer(function(player)
 	local player_name = player:get_player_name()
 	local pos = player:get_pos()
-	
+
 	pos.x = math.floor(pos.x + 0.5)
 	pos.y = math.floor(pos.y + 0.5)
 	pos.z = math.floor(pos.z + 0.5)
