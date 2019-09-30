45090472b998ff033dd64a5d5c29a0df38164357
OgelGames <>
2019-09-30 23:50:19 +1000

    Better fix for bones in nuclear reactor

--- technic.lua
+++ technic.lua
@@ -1,15 +1,30 @@
 
 
 -- https://github.com/pandorabox-io/pandorabox.io/issues/330
--- replaces bones in an active reactor with a water source
 minetest.register_abm({
-        label = "liquify bones in active reactors",
+        label = "move or drop bones in active reactors",
         nodenames = {"bones:bones"},
         neighbors = {"technic:hv_nuclear_reactor_core_active"},
         interval = 2,
         chance = 1,
         action = function(pos)
-		minetest.set_node(pos, { name="default:water_source" })
+			-- try to find nearby space to move bones into
+			local newpos = minetest.find_node_near(pos, 1, { "air", "vacuum:vacuum"})
+			if newpos ~= nil then
+				local meta = minetest.get_meta(pos):to_table()
+				minetest.set_node(newpos, { name = "bones:bones" })
+				minetest.get_meta(newpos):from_table(meta)
+			else
+			-- otherwise drop inventory and bones
+				local inv = minetest.get_meta(pos):get_inventory()
+				for i = 1, inv:get_size("main") do
+					local stk = inv:get_stack("main", i)
+					inv:set_stack("main", i, nil)
+					minetest.add_item(pos, stk:take_item(stk:get_count()))
+				end
+				minetest.add_item(pos, "bones:bones")
+			end
+			minetest.set_node(pos, { name = "default:water_source" })
         end
 })
 
