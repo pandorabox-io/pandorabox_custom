b8b2f9c17c26f56b8e6d6db8de67cd8835fe4693
Luke aka SwissalpS <Luke@SwissalpS.ws>
2019-10-30 03:40:46 +0100

    Propose a simple approach to help find bones

--- death.lua
+++ death.lua
@@ -1,4 +1,5 @@
 
+local BONES_WAYPOINT_EXPIRES_SECONDS = 42 * 60
 
 minetest.register_on_dieplayer(function(player)
 	local player_name = player:get_player_name()
@@ -10,4 +11,15 @@ minetest.register_on_dieplayer(function(player)
 
 	minetest.log("action", "[death] player '" .. player_name .. "' died at " .. minetest.pos_to_string(pos))
 	minetest.chat_send_player(player_name, "You died at " .. minetest.pos_to_string(pos))
+
+	local hud_id = player:hud_add({
+		hud_elem_type = "waypoint",
+		name = "Bones of " .. player_name,
+		text = "m",
+		number = 0xFFFFFF,
+		world_pos = pos
+	})
+
+	minetest.after(BONES_WAYPOINT_EXPIRES_SECONDS, function() player:hud_remove(hud_id) end)
+
 end)
