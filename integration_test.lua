caf74999427c3c180e19c05372d1ad513c358acf
NatureFreshMilk <naturefreshmilk@github.com>
2020-01-14 12:57:48 +0100

    add asserts

--- integration_test.lua
+++ integration_test.lua
@@ -1,24 +1,54 @@
 
 minetest.log("warning", "[pandorabox_custom] integration-test enabled!")
 
+-- those mods have to be present
+local assert_mods = {
+	"advtrains",
+	"technic",
+	"pipeworks",
+	"moreblocks"
+}
+
+-- those nodes have to be present
+local assert_nodes = {
+	"jumpdrive:engine"
+}
+
+-- defered integration test function
 minetest.register_on_mods_loaded(function()
 	minetest.log("warning", "[pandorabox_custom] all mods loaded, starting delayed test-function")
 
 	minetest.after(1, function()
 		minetest.log("warning", "[pandorabox_custom] starting integration test")
 
-		local data = minetest.write_json({ success = true }, true);
-		local file = io.open(minetest.get_worldpath().."/integration_test.json", "w" );
-		if file then
-			file:write(data)
-			file:close()
-		end
-
-		file = io.open(minetest.get_worldpath().."/registered_nodes.txt", "w" );
+		-- export stats
+		local file = io.open(minetest.get_worldpath().."/registered_nodes.txt", "w" );
 		if file then
 			for name in pairs(minetest.registered_nodes) do
 				file:write(name .. '\n')
 			end
+			file:close()
+		end
+
+		-- check mods
+		for _, modname in ipairs(assert_mods) do
+			if not minetest.get_modpath(modname) then
+				error("Mod not present: " .. modname)
+			end
+		end
+
+		-- check nodes
+		for _, nodename in ipairs(assert_nodes) do
+			if not minetest.registered_nodes[nodename] then
+				error("Node not present: " .. nodename)
+			end
+		end
+
+		-- write success flag
+		local data = minetest.write_json({ success = true }, true);
+		file = io.open(minetest.get_worldpath().."/integration_test.json", "w" );
+		if file then
+			file:write(data)
 			file:close()
 		end
 
