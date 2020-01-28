547577ba376f16d7ef8405e0be895ec33a0abd3e
NatureFreshMilk <naturefreshmilk@github.com>
2020-01-28 11:17:02 +0100

    move moon skybox to planet_moon

--- skybox.lua
+++ skybox.lua
@@ -1,20 +1,4 @@
 
--- space
-skybox.register({
-	name = "space",
-	miny = 1000,
-	maxy = 5000,
-	textures = {"space_sky.png","space_sky2.png","space_sky.png","space_sky.png","space_sky.png","space_sky.png"}
-})
-
--- moon
-skybox.register({
-	name = "moon",
-	miny = 5001,
-	maxy = 6000,
-	always_day = true,
-	textures = {"space_sky.png","space_sky.png","space_sky.png","space_sky.png","space_sky.png","space_sky.png"}
-})
 
 
 -- deep space
