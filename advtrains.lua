9b145d835fc23f01f0df3f8364a1dc7d5366ba12
NatureFreshMilk <naturefreshmilk@github.com>
2020-02-03 13:47:07 +0100

    advtrains.path_get_index_by_offset

--- advtrains.lua
+++ advtrains.lua
@@ -1,23 +1,22 @@
 
 local cache = {}
 
+-- advtrains.path_get (dows not work, sideeffects)
+-- advtrains.get_rail_info_at (works, no improvement)
 
-local old_get_rail_info_at = advtrains.get_rail_info_at
+-- advtrains.path_get_index_by_offset
+
+
+local old_path_get_index_by_offset = advtrains.path_get_index_by_offset
 
 -- memoize function
-advtrains.get_rail_info_at = function(pos, drives_on)
-  local key = math.floor(pos.x) .. "/" .. math.floor(pos.y) .. "/" .. math.floor(pos.z) .. "/"
-  if drives_on then
-    key = key .. "true"
-  else
-    key = key .. "false"
-  end
-
+advtrains.path_get_index_by_offset = function(train, index, offset)
+  local key = train.id .. "/" .. index .. "/" .. offset
   local data = cache[key]
   if data then
     return data[1], data[2], data[3]
   else
-    local r1, r2, r3 = old_get_rail_info_at(pos, drives_on)
+    local r1, r2, r3 = old_path_get_index_by_offset(train, index. offset)
     cache[key] = { r1, r2, r3 }
     return r1, r2, r3
   end
