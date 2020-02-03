dc23b09a168897a7c461bdbc26f3a1cd7eeb72e7
NatureFreshMilk <naturefreshmilk@github.com>
2020-02-03 11:25:36 +0100

    store both return values

--- advtrains.lua
+++ advtrains.lua
@@ -7,13 +7,19 @@ local old_path_get = advtrains.path_get
 -- memoize path_get function
 advtrains.path_get = function(train, index)
   local key = train.id .. "/" .. index
-  local result = cache[key]
-  if not result then
-    result = old_path_get(train, index)
-    cache[key] = result
+  local result1, result2
+  local data = cache[key]
+  if not data then
+    -- not cached
+    result1, result2 = old_path_get(train, index)
+    cache[key] = { result1, result2 }
+  else
+    -- cached
+    result1 = data[1]
+    result2 = data[1]
   end
 
-  return result
+  return result1, result2
 end
 
 
