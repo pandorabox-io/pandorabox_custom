cc9d4942cb7adfe451ed6b091a25f45409d097c2
NatureFreshMilk <naturefreshmilk@github.com>
2020-02-03 13:22:26 +0100

    old_get_rail_info_at

--- advtrains.lua
+++ advtrains.lua
@@ -2,24 +2,25 @@
 local cache = {}
 
 
-local old_path_get = advtrains.path_get
+local old_get_rail_info_at = advtrains.get_rail_info_at
 
--- memoize path_get function
-advtrains.path_get = function(train, index)
-  local key = train.id .. "/" .. index
-  local result1, result2
-  local data = cache[key]
-  if not data then
-    -- not cached
-    result1, result2 = old_path_get(train, index)
-    cache[key] = { result1, result2 }
+-- memoize function
+advtrains.get_rail_info_at = function(pos, drives_on)
+  local key = pos.x .. "/" .. pos.y .. "/" .. pos.z .. "/"
+  if drives_on then
+    key = key + "true"
   else
-    -- cached
-    result1 = data[1]
-    result2 = data[2]
+    key = key + "false"
   end
 
-  return result1, result2
+  local data = cache[key]
+  if data then
+    return data[1], data[2], data[3]
+  else
+    local r1, r2, r3 = old_get_rail_info_at(pos, drives_on)
+    cache[key] = { r1, r2, r3 }
+    return r1, r2, r3
+  end
 end
 
 local old_mainloop_trainlogic = advtrains.mainloop_trainlogic
