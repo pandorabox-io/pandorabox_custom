ad9ef6404f5bc44fcc849067ae22a188e0131d74
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-03-16 10:13:33 +0100

    try force-registering node registration

--- advtrains_platform.lua
+++ advtrains_platform.lua
@@ -1,14 +1,14 @@
 
 if minetest.get_modpath("prefab") then
-  advtrains.register_platform("advtrains_platform", "prefab:concrete")
+  advtrains.register_platform(":advtrains_platform", "prefab:concrete")
 end
 
 
 if minetest.get_modpath("scifi_nodes") then
-  advtrains.register_platform("advtrains_platform", "scifi_nodes:white")
-  advtrains.register_platform("advtrains_platform", "scifi_nodes:white2")
-  advtrains.register_platform("advtrains_platform", "scifi_nodes:tile")
-  advtrains.register_platform("advtrains_platform", "scifi_nodes:whttl")
-  advtrains.register_platform("advtrains_platform", "scifi_nodes:metal")
-  advtrains.register_platform("advtrains_platform", "scifi_nodes:wall")
+  advtrains.register_platform(":advtrains_platform", "scifi_nodes:white")
+  advtrains.register_platform(":advtrains_platform", "scifi_nodes:white2")
+  advtrains.register_platform(":advtrains_platform", "scifi_nodes:tile")
+  advtrains.register_platform(":advtrains_platform", "scifi_nodes:whttl")
+  advtrains.register_platform(":advtrains_platform", "scifi_nodes:metal")
+  advtrains.register_platform(":advtrains_platform", "scifi_nodes:wall")
 end
