6d2271b56cf43ba4bf6fa643316563814a6de57c
OgelGames <olliverdc28@gmail.com>
2020-03-21 21:26:04 +1100

    add super fertiliser

--- bonemeal.lua
+++ bonemeal.lua
@@ -5,3 +5,36 @@ bonemeal:add_deco({
 	{ "default:dirt_with_rainforest_litter",
 		{"default:junglegrass", "", ""}, {} }
 })
+
+
+-- super fertiliser (strength 4)
+-- uses charcoal as a soil amendment
+-- https://en.wikipedia.org/wiki/Biochar
+if minetest.get_modpath("charcoal") then
+
+	local creative_mode = minetest.settings:get_bool("creative_mode")
+
+	minetest.register_craftitem("pandorabox_custom:super_fertiliser", {
+		description = "Super Fertiliser",
+		inventory_image = "bonemeal_item.png^[multiply:#00a000ff",
+		on_use = function(itemstack, user, pointed_thing)
+			if pointed_thing.type ~= "node" then
+				return
+			end
+			if minetest.is_protected(pointed_thing.under, user:get_player_name()) then
+				return
+			end
+			if not creative_mode or not minetest.check_player_privs(user:get_player_name(), {creative = true}) then
+				itemstack:take_item()
+			end
+			bonemeal:on_use(pointed_thing.under, 4)
+			return itemstack
+		end
+	})
+
+	minetest.register_craft({
+		type = "shapeless",
+		output = "pandorabox_custom:super_fertiliser",
+		recipe = {"bonemeal:fertiliser", "charcoal:charcoal_lump"}
+	})
+end
