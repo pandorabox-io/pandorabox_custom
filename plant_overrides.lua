fcaa42a9b682664c364074dda2730c98766347c1
Victor Gridnevsky <vic.6r1d@gmail.com>
2020-05-14 21:28:29 +0300

    makes bamboo leaves climbable

--- plant_overrides.lua
+++ plant_overrides.lua
@@ -1,7 +1,12 @@
 -- This file contains plantlife-related overrides
 
--- Players should be able to climb bamboo trunks
+-- Players should be able to climb bamboo trunks and leaves
 minetest.override_item("bamboo:trunk", {
+	walkable = false,
+	climbable = true
+})
+
+minetest.override_item("bamboo:leaves", {
 	walkable = false,
 	climbable = true
 })
