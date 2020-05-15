712b1291748e41b6179de8516f08322a996ad5b2
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-05-15 07:40:49 +0200

    apply workaround/hack related to https://github.com/minetest/minetest/issues/9849

--- protector_forceload.lua
+++ protector_forceload.lua
@@ -22,3 +22,26 @@ protector.can_dig = function(radius, pos, digger, onlyowner, infolevel)
 
   return old_can_dig(radius, pos, digger, onlyowner, infolevel)
 end
+
+
+-- additionally do periodic forceloads from cache
+-- near players
+-- https://github.com/minetest/minetest/issues/9849
+-- https://gitlab.com/h2mm/mapload_boost/-/blob/master/init.lua
+
+local reload_period = 1
+local time_left = reload_period + 10
+local load_radius = 16
+
+minetest.register_globalstep(function(dtime)
+	time_left = time_left - dtime
+	if time_left < 0 then
+		time_left = reload_period
+		for _, player in ipairs(minetest.get_connected_players()) do
+			minetest.get_voxel_manip(
+        vector.subtract(player:get_pos(), load_radius),
+        vector.subtract(player:get_pos(), load_radius)
+      )
+		end
+	end
+end)
