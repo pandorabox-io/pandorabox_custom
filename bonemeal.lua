c76acc64f6d5e2a3db87bb8e1c9f39d377e21c95
OgelGames <olliverdc28@gmail.com>
2020-05-18 19:54:15 +1000

    make more plants obtainable by mulching

--- bonemeal.lua
+++ bonemeal.lua
@@ -1,9 +1,19 @@
 
--- makes junglegrass obtainable by mulching
+-- make more plants obtainable by mulching
 -- junglegrass is used to make many nodes, such as moreblocks:clean_glass
 bonemeal:add_deco({
 	{ "default:dirt_with_rainforest_litter",
-		{"default:junglegrass", "", ""}, {} }
+		{"default:junglegrass", ""}, {} }
+})
+
+bonemeal:add_deco({
+	{ "default:dirt_with_coniferous_litter",
+		{}, {"default:fern_1", "default:fern_2", "default:fern_3", "", ""} }
+})
+
+bonemeal:add_deco({
+	{ "default:sand",
+		{"default:marram_grass_1", "default:marram_grass_2", "default:marram_grass_3", "", ""}, {} }
 })
 
 
