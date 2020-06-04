0cc4812a0ceb4e764e296472a54e85363f2ff861
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-06-04 13:41:37 +0200

    rework xp based autogrant, add onjoin check and optimize a bit

--- xp_priv.lua
+++ xp_priv.lua
@@ -18,17 +18,57 @@ local privs = {
 	{ xp = 500000, name = "areas_high_limit" }
 }
 
+-- playername -> time_of_last_check
+local last_check_map = {}
 
+local function check_autogrant(playername, xp)
+	local now = os.time()
+	local last_check = last_check_map[playername]
+
+	if last_check and (now - last_check) < 2 then
+		-- check was executed shortly before, wait a bit to re-check again
+		-- in case player is using a digtron or a fast tool
+		return
+	end
+
+	last_check_map[playername] = now
+
+	-- get player priv map
+	local player_privs = minetest.get_player_privs(playername)
+
+	-- dirty flag
+	local changed = false
+
+	-- loop over privs and grant priv if xp reached and not granted yet
+	for _, p in ipairs(privs) do
+		if xp > p.xp and not player_privs[p.name] then
+			-- xp threshold exceeded and priv not yet granted
+			player_privs[p.name] = true
+			changed = true
+			minetest.chat_send_player(playername, "XP Granted priv acquired: " .. p.name)
+		end
+	end
+
+	if changed then
+		-- apply changed privs
+		minetest.set_player_privs(playername, player_privs)
+	end
+end
+
+-- add xp_redo hook
 xp_redo.register_hook({
-  xp_change = function(playername, xp)
-    for _, p in ipairs(privs) do
-      if xp > p.xp and not minetest.check_player_privs(playername, p.name) then
-        -- xp threshold exceeded and priv not yet granted
-        local player_privs = minetest.get_player_privs(playername)
-        player_privs[p.name] = true
-        minetest.set_player_privs(playername, player_privs)
-        minetest.chat_send_player(playername, "XP Granted priv acquired: " .. p.name)
-      end
-    end
-  end
+  xp_change = check_autogrant
 })
+
+-- check privs upon joining
+minetest.register_on_joinplayer(function(player)
+	local playername = player:get_player_name()
+	local xp = xp_redo.get_xp(playername)
+	check_autogrant(playername, xp)
+end)
+
+-- clear last_check cache
+minetest.register_on_leaveplayer(function(player)
+	local playername = player:get_player_name()
+	last_check_map[playername] = nil
+end)
