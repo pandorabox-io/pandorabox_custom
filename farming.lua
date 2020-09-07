8f43b92328122fb409b272bfd09049de99cbd60a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-09-07 08:23:53 +0200

    swap blueberry names
    relevant: https://github.com/pandorabox-io/pandorabox-mods/pull/997

--- farming.lua
+++ farming.lua
@@ -9,3 +9,20 @@ minetest.register_craftitem(":farming:dough", {
   description = "Dough",
   inventory_image = "farming_dough.png",
 })
+
+-- swap farming/default blueberry names
+-- See: https://github.com/pandorabox-io/pandorabox-mods/pull/997
+
+--[[
+Comment by @OgelGames:
+> default:blueberries should be wild, because they grow on bushes,
+> and farming:blueberries should not be wild because they grow as a small plant
+--]]
+
+minetest.override_item("default:blueberries", {
+  description = farming.intllib("Blueberries")
+})
+
+minetest.override_item("farming:blueberries", {
+  description = default.get_translator("Blueberries")
+})
