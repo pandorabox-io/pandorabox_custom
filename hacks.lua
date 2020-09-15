6aa53d8b1297b60f00dc649306469c1ef15bdc5d
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2020-09-15 06:47:20 +0200

    apply override suggestion from @S-S-X
    Related: https://github.com/pandorabox-io/pandorabox.io/issues/539#issuecomment-692273573

--- hacks.lua
+++ hacks.lua
@@ -20,3 +20,8 @@ end
 -- there are still problems with the ipv6 range comming in from a single ipv4 ip
 minetest.unregister_chatcommand("ban")
 
+-- disable digiline functionality temporarily
+-- issue: https://github.com/pandorabox-io/pandorabox.io/issues/539
+if minetest.get_modpath("digistuff") then
+	minetest.override_item("digistuff:nic",{ digiline = { receptor = {}, effector = {} } })
+end
