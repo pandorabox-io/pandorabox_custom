3916c3bff81c4befc08548e28de9f5a28b7d60d1
OgelGames <olliverdc28@gmail.com>
2020-09-16 00:02:23 +1000

    check if msg is a string

--- hacks.lua
+++ hacks.lua
@@ -29,6 +29,7 @@ if minetest.get_modpath("digistuff") and minetest.registered_nodes["digistuff:ni
 			receptor = {},
 			effector = {
 				action = function(pos,node,channel,msg)
+					if type(msg) ~= "string" then return end
 					msg = string.gsub(msg, "%s", "%%20")
 					return old_action(pos,node,channel,msg)
 				end
