9028b638689d9144041d932a513a773ce72563fe
Luke aka SwissalpS <Luke@SwissalpS.ws>
2020-10-12 06:26:49 +0200

    Update digi_copier_fixer.lua
    missing supply converter and forcefield formspec updaters, otherwise seems to work.

--- digi_copier_fixer.lua
+++ digi_copier_fixer.lua
@@ -1,84 +1,140 @@
-local additionalnodes = {
-	["beacon:black"] = "channel",
-	["beacon:blue"] = "channel",
-	["beacon:cyan"] = "channel",
-	["beacon:green"] = "channel",
-	["beacon:magenta"] = "channel",
-	["beacon:orange"] = "channel",
-	["beacon:red"] = "channel",
-	["beacon:violet"] = "channel",
-	["beacon:white"] = "channel",
-	["beacon:yellow"] = "channel",
-	["digiline_global_memory:controller"] = "channel",
-	["digiline_routing:filter"] = "channel",
-	["digilines:chest"] = "channel",
-	["digilines:lcd"] = "channel",
-	["digilines:lightsensor"] = "channel",
-	["digilines:rtc"] = "channel",
-	["digiterms:beige_keyboard"] = "channel",
-	["digiterms:black_keyboard"] = "channel",
-	["digiterms:cathodic_beige_monitor"] = "channel",
-	["digiterms:cathodic_black_monitor"] = "channel",
-	["digiterms:cathodic_white_monitor"] = "channel",
-	["digiterms:lcd_monitor"] = "channel",
-	["digiterms:scifi_glassscreen"] = "channel",
-	["digiterms:scifi_keysmonitor"] = "channel",
-	["digiterms:scifi_tallscreen"] = "channel",
-	["digiterms:scifi_widescreen"] = "channel",
-	["digiterms:white_keyboard"] = "channel",
-	["drawers:controller"] = "digilineChannel",
-	["jumpdrive:engine"] = "channel",
-	["jumpdrive:fleet_controller"] = "channel",
-	["monitoring_digilines:metric_controller"] = "channel",
-	["pipeworks:autocrafter"] = "channel",
-	["pipeworks:digiline_detector_tube_1"] = "channel",
-	["pipeworks:digiline_detector_tube_2"] = "channel",
-	["pipeworks:digiline_detector_tube_3"] = "channel",
-	["pipeworks:digiline_detector_tube_4"] = "channel",
-	["pipeworks:digiline_detector_tube_5"] = "channel",
-	["pipeworks:digiline_detector_tube_6"] = "channel",
-	["pipeworks:digiline_detector_tube_7"] = "channel",
-	["pipeworks:digiline_detector_tube_8"] = "channel",
-	["pipeworks:digiline_detector_tube_9"] = "channel",
-	["pipeworks:digiline_detector_tube_10"] = "channel",
-	["pipeworks:digiline_filter"] = "channel",
-	["technic:forcefield_emitter_off"] = "channel",
-	["technic:forcefield_emitter_on"] = "channel",
-	["technic:hv_battery_box0"] = "channel",
-	["technic:hv_battery_box1"] = "channel",
-	["technic:hv_battery_box2"] = "channel",
-	["technic:hv_battery_box3"] = "channel",
-	["technic:hv_battery_box4"] = "channel",
-	["technic:hv_battery_box5"] = "channel",
-	["technic:hv_battery_box6"] = "channel",
-	["technic:hv_battery_box7"] = "channel",
-	["technic:hv_battery_box8"] = "channel",
-	["technic:hv_nuclear_reactor_core"] = "remote_channel",
-	["technic:hv_nuclear_reactor_core_active"] = "remote_channel",
-	["technic:lv_battery_box0"] = "channel",
-	["technic:lv_battery_box1"] = "channel",
-	["technic:lv_battery_box2"] = "channel",
-	["technic:lv_battery_box3"] = "channel",
-	["technic:lv_battery_box4"] = "channel",
-	["technic:lv_battery_box5"] = "channel",
-	["technic:lv_battery_box6"] = "channel",
-	["technic:lv_battery_box7"] = "channel",
-	["technic:lv_battery_box8"] = "channel",
-	["technic:mv_battery_box0"] = "channel",
-	["technic:mv_battery_box1"] = "channel",
-	["technic:mv_battery_box2"] = "channel",
-	["technic:mv_battery_box3"] = "channel",
-	["technic:mv_battery_box4"] = "channel",
-	["technic:mv_battery_box5"] = "channel",
-	["technic:mv_battery_box6"] = "channel",
-	["technic:mv_battery_box7"] = "channel",
-	["technic:mv_battery_box8"] = "channel",
-	["technic:quarry"] = "channel",
-	["textline:lcd"] = "channel",
-}
+local function apply_overrides()
+	local additionalnodes = {
+		["beacon:black"] = "channel",
+		["beacon:blue"] = "channel",
+		["beacon:cyan"] = "channel",
+		["beacon:green"] = "channel",
+		["beacon:magenta"] = "channel",
+		["beacon:orange"] = "channel",
+		["beacon:red"] = "channel",
+		["beacon:violet"] = "channel",
+		["beacon:white"] = "channel",
+		["beacon:yellow"] = "channel",
+		["digiline_global_memory:controller"] = "channel",
+		["digiline_routing:filter"] = "channel", -- infotext is not updated
+		["digilines:chest"] = "channel",
+		["digilines:lcd"] = "channel",
+		["digilines:lightsensor"] = "channel",
+		["digilines:rtc"] = "channel",
+		["digiterms:beige_keyboard"] = "channel",
+		["digiterms:black_keyboard"] = "channel",
+		["digiterms:cathodic_beige_monitor"] = "channel",
+		["digiterms:cathodic_black_monitor"] = "channel",
+		["digiterms:cathodic_white_monitor"] = "channel",
+		["digiterms:lcd_monitor"] = "channel",
+		["digiterms:scifi_glassscreen"] = "channel",
+		["digiterms:scifi_keysmonitor"] = "channel",
+		["digiterms:scifi_tallscreen"] = "channel",
+		["digiterms:scifi_widescreen"] = "channel",
+		["digiterms:white_keyboard"] = "channel",
+		["drawers:controller"] = "digilineChannel",
+		["jumpdrive:engine"] = "channel", -- formspec is not updated (reset button does it)
+		["jumpdrive:fleet_controller"] = "channel", -- formspec is not updated (reset button does it)
+		["monitoring_digilines:metric_controller"] = "channel",
+		["pipeworks:autocrafter"] = "channel",
+		["pipeworks:digiline_detector_tube_1"] = "channel",
+		["pipeworks:digiline_detector_tube_2"] = "channel",
+		["pipeworks:digiline_detector_tube_3"] = "channel",
+		["pipeworks:digiline_detector_tube_4"] = "channel",
+		["pipeworks:digiline_detector_tube_5"] = "channel",
+		["pipeworks:digiline_detector_tube_6"] = "channel",
+		["pipeworks:digiline_detector_tube_7"] = "channel",
+		["pipeworks:digiline_detector_tube_8"] = "channel",
+		["pipeworks:digiline_detector_tube_9"] = "channel",
+		["pipeworks:digiline_detector_tube_10"] = "channel",
+		["pipeworks:digiline_filter"] = "channel",
+		["technic:forcefield_emitter_off"] = "channel", -- formspec is not updated
+		["technic:forcefield_emitter_on"] = "channel", -- formspec is not updated
+		["technic:hv_battery_box0"] = "channel",
+		["technic:hv_battery_box1"] = "channel",
+		["technic:hv_battery_box2"] = "channel",
+		["technic:hv_battery_box3"] = "channel",
+		["technic:hv_battery_box4"] = "channel",
+		["technic:hv_battery_box5"] = "channel",
+		["technic:hv_battery_box6"] = "channel",
+		["technic:hv_battery_box7"] = "channel",
+		["technic:hv_battery_box8"] = "channel",
+		["technic:hv_nuclear_reactor_core"] = "remote_channel",
+		["technic:hv_nuclear_reactor_core_active"] = "remote_channel",
+		["technic:lv_battery_box0"] = "channel",
+		["technic:lv_battery_box1"] = "channel",
+		["technic:lv_battery_box2"] = "channel",
+		["technic:lv_battery_box3"] = "channel",
+		["technic:lv_battery_box4"] = "channel",
+		["technic:lv_battery_box5"] = "channel",
+		["technic:lv_battery_box6"] = "channel",
+		["technic:lv_battery_box7"] = "channel",
+		["technic:lv_battery_box8"] = "channel",
+		["technic:mv_battery_box0"] = "channel",
+		["technic:mv_battery_box1"] = "channel",
+		["technic:mv_battery_box2"] = "channel",
+		["technic:mv_battery_box3"] = "channel",
+		["technic:mv_battery_box4"] = "channel",
+		["technic:mv_battery_box5"] = "channel",
+		["technic:mv_battery_box6"] = "channel",
+		["technic:mv_battery_box7"] = "channel",
+		["technic:mv_battery_box8"] = "channel",
+		["technic:quarry"] = "channel",
+		["technic:supply_converter"] = "channel",  -- formspec is not updated
+		["technic:switching_station"] = "channel",
+		["textline:lcd"] = "channel",
+	}
 
-for name,field in pairs(additionalnodes) do
-	if minetest.registered_nodes[name] and not minetest.registered_nodes[name]._digistuff_channelcopier_fieldname then
-		minetest.override_item(name,{_digistuff_channelcopier_fieldname = field})
+	for name, field in pairs(additionalnodes) do
+		if minetest.registered_nodes[name]
+			and not minetest.registered_nodes[name]._digistuff_channelcopier_fieldname then
+				minetest.override_item(name,{ _digistuff_channelcopier_fieldname = field })
+		end
 	end
+
+	-- after channel change, some need special attention
+
+	-- routing filter
+	local osRoutingFilter = function(pos, node, player, new_channel, old_channel)
+		local FILTER_INFOTEXT = "Digiline Filter (channel \"%s\")"
+		local meta = minetest.get_meta(pos)
+		meta:set_string("infotext", FILTER_INFOTEXT:format(meta:get_string("channel")))
+	end
+
+	-- jumpdrive engine
+	local osJumpdrive = function(pos, node, player, new_channel, old_channel)
+		local meta = minetest.get_meta(pos)
+		jumpdrive.migrate_engine_meta(pos, meta)
+		jumpdrive.update_formspec(meta, pos)
+	end
+	-- jumpdrive fleet controller
+	local osFleetController = function(pos, node, player, new_channel, old_channel)
+		local meta = minetest.get_meta(pos)
+		jumpdrive.fleet.update_formspec(meta, pos)
+	end
+
+	-- force field
+	local osForceField = function(pos, node, player, new_channel, old_channel)
+		local meta = minetest.get_meta(pos)
+		-- TODO: This one is more complex
+	end
+
+	-- supply converter
+	local osForceField = function(pos, node, player, new_channel, old_channel)
+		local meta = minetest.get_meta(pos)
+		-- TODO: This one is more complex
+	end
+
+	local onsets = {
+		["digiline_routing:filter"] = osRoutingFilter,
+		["jumpdrive:engine"] = osJumpdrive,
+		["jumpdrive:fleet_controller"] = osFleetController,
+		["technic:forcefield_emitter_off"] = osForceField,
+		["technic:forcefield_emitter_on"] = osForceField,
+		["technic:supply_converter"] = osSupplyConverter,
+	}
+
+	for name, field in pairs(onsets) do
+		if minetest.registered_nodes[name] and not minetest.registered_nodes[name]._digistuff_channelcopier_onset then
+			minetest.override_item(name,{ _digistuff_channelcopier_onset = field })
+		end
+	end
+
 end
+
+apply_overrides()
