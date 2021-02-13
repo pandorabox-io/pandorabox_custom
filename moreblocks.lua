6f64e06958f30fb7e40c7b44fa75f8495fcbb301
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-02-13 12:05:14 +0100

    add recipe / refactor code

--- moreblocks.lua
+++ moreblocks.lua
@@ -1,27 +1,31 @@
-local sound_gravel = default.node_sound_gravel_defaults()
 
--- sanity checks
-assert(sound_gravel)
+-- half gravel, half stonebrick
+minetest.register_node(":moreblocks:gravel_stonebrick", {
+	description = "Gravel on Stonebrick",
+	tiles = {
+		"default_gravel.png",
+		"default_stone_brick.png",
+		"default_gravel.png^[lowpart:50:default_stone_brick.png",
+		"default_gravel.png^[lowpart:50:default_stone_brick.png",
+		"default_gravel.png^[lowpart:50:default_stone_brick.png",
+		"default_gravel.png^[lowpart:50:default_stone_brick.png"
+	},
+	no_stairs = true,
+	groups = {cracky = 3},
+	sounds = default.node_sound_gravel_defaults()
+})
 
--- custom node table
-local nodes = {
-	["gravel_stonebrick"] = {
-		description = "Gravel on Stonebrick",
-		tiles = {
-			"default_gravel.png",
-			"default_stone_brick.png",
-			"default_gravel.png^[lowpart:50:default_stone_brick.png",
-			"default_gravel.png^[lowpart:50:default_stone_brick.png",
-			"default_gravel.png^[lowpart:50:default_stone_brick.png",
-			"default_gravel.png^[lowpart:50:default_stone_brick.png"
-		},
-		no_stairs = true,
-		groups = {cracky = 3},
-		sounds = sound_gravel,
-	}
-}
+stairsplus:register_all(
+	"moreblocks",
+	"gravel_stonebrick",
+	"moreblocks:gravel_stonebrick",
+	minetest.registered_nodes["moreblocks:gravel_stonebrick"]
+)
 
-for name, def in pairs(nodes) do
-	minetest.register_node(":moreblocks:" .. name, def)
-	stairsplus:register_all("moreblocks", name,  "moreblocks:" .. name, def)
-end
+minetest.register_craft({
+    output = "moreblocks:gravel_stonebrick 4",
+    recipe = {
+        {"default:gravel", "default:gravel"},
+        {"default:stonebrick", "default:stonebrick"},
+    }
+})
