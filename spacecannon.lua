c9135f0718e14584282a39321e9f1bd9b689f529
Dennis Jenkins <dennis.jenkins.75@gmail.com>
2021-02-25 04:28:23 -0800

    Also allow spacecannon use deep underground.

--- spacecannon.lua
+++ spacecannon.lua
@@ -1,5 +1,5 @@
 
 spacecannon.can_shoot = function(pos)
-	-- only allow shooting in space
-	return pos.y > 1000
+	-- only allow shooting in space or deep underground
+	return (pos.y > 1000) or (pos.y < 1000)
 end
