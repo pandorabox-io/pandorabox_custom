778b6e8b8c3badb7da0cb3447a5f91b58228e601
SX <50966843+S-S-X@users.noreply.github.com>
2021-03-06 19:46:57 +0200

    Fix check player privs

--- spacecannon.lua
+++ spacecannon.lua
@@ -1,6 +1,6 @@
 
 spacecannon.can_shoot = function(pos, playername)
-	local has_unrestricted_priv = playername and minetest.check_player_priv(playername, "spacecannon_unrestricted")
+	local has_unrestricted_priv = playername and minetest.check_player_privs(playername, "spacecannon_unrestricted")
 	-- only allow shooting in space or with priv
 	return has_unrestricted_priv or pos.y > 1000
 end
