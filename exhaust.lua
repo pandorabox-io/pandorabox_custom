0cd61729f7d995679ad089347257b3993bf7d02f
SX <50966843+S-S-X@users.noreply.github.com>
2021-05-23 23:16:22 +0300

    Fix exhaust

--- exhaust.lua
+++ exhaust.lua
@@ -1,16 +1,16 @@
 local timestamp = {}
-stamina.register_on_exhaust_player(function(player)
-	if not player then return end
+stamina.register_on_exhaust_player(function(player, change)
+	if not player or change < stamina.settings.exhaust_craft then return end
 	local now = minetest.get_us_time()
-	if timestamp[player:get_player_name()] then
-		if now - timestamp[player:get_player_name()] > 999999 then
-			local exhaustion = stamina.get_exhaustion(player) or 0
-			if exhaustion >= stamina.settings.exhaust_lvl then
-				timestamp[player:get_player_name()] = now
+	local name = player:get_player_name()
+	if timestamp[name] then
+		if now - timestamp[name] > 999999 then
+			if (stamina.get_saturation(player) or 0) <= stamina.settings.starve_lvl then
+				timestamp[name] = now
 				player:set_hp(player:get_hp() - 1, "exhaust")
 			end
 		end
 	else
-		timestamp[player:get_player_name()] = now
+		timestamp[name] = now
 	end
 end)
