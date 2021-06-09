1d1e009019a1a1484648a0631cdc398bb05de933
OgelGames <48543043+OgelGames@users.noreply.github.com>
2021-06-09 17:17:42 +1000

    remove melon buildable_to override
    fixed upstream in https://notabug.org/TenPlus1/farming/commit/4b033fed838de1f936b808d4ab6d10a70682c02c

--- farming.lua
+++ farming.lua
@@ -1,9 +1,4 @@
 
--- https://github.com/pandorabox-io/pandorabox.io/issues/298
-minetest.override_item("farming:melon_8", {
-  buildable_to = false
-})
-
 -- Dough craftitem
 minetest.register_craftitem(":farming:dough", {
   description = "Dough",
