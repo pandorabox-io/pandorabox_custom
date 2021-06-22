f5e77198488d2b1edf7389f6822732783919e008
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-06-22 10:32:54 +0200

    remove nic override

--- hacks.lua
+++ hacks.lua
@@ -20,24 +20,6 @@ end
 -- there are still problems with the ipv6 range comming in from a single ipv4 ip
 minetest.unregister_chatcommand("ban")
 
--- fix digiline nic functionality temporarily
--- issue: https://github.com/pandorabox-io/pandorabox.io/issues/539
-if minetest.get_modpath("digistuff") and minetest.registered_nodes["digistuff:nic"] then
-	local old_action = minetest.registered_nodes["digistuff:nic"].digiline.effector.action
-	minetest.override_item("digistuff:nic",{
-		digiline = {
-			receptor = {},
-			effector = {
-				action = function(pos,node,channel,msg)
-					if type(msg) ~= "string" then return end
-					msg = string.gsub(msg, "%s", "%%20")
-					return old_action(pos,node,channel,msg)
-				end
-			}
-		}
-	})
-end
-
 -- prevent big towers of papyrus and bamboo from being auto-dug
 -- https://github.com/pandorabox-io/pandorabox.io/issues/611
 function default.dig_up(pos, node, digger)
