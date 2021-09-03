5bd9f11f8338ac2f43d5b50fb3044c22d29e56e5
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2021-09-03 09:58:27 +0200

    Update beerchat.lua
    Co-authored-by: SX <50966843+S-S-X@users.noreply.github.com>

--- beerchat.lua
+++ beerchat.lua
@@ -1,7 +1,6 @@
 
 beerchat.register_callback("before_join", function(name, channel)
-        if channel == "mod" then
-                local success = minetest.check_player_privs(name, "staff")
-                return success
+        if channel == "mod" and not minetest.check_player_privs(name, "staff") then
+                return false
         end
 end)
