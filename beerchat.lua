76e9d692e59f005992391aff739408ea6497cca5
SX <50966843+S-S-X@users.noreply.github.com>
2021-09-30 22:16:20 +0300

    advtrains debug messages to advtrains channel

--- beerchat.lua
+++ beerchat.lua
@@ -4,3 +4,18 @@ beerchat.register_callback("before_join", function(name, channel)
                 return false
         end
 end)
+
+atwarn = function(t, ...)
+	local text = advtrains.print_concat_table({t, ...})
+	minetest.log("warning", "[advtrains]"..text)
+	--minetest.chat_send_all("[advtrains] -!- "..text)
+	beerchat.send_on_channel("[advtrains] -!- ", "advtrains", text)
+end
+
+--ONLY use this function for temporary debugging. for consistent debug prints use atprint
+atdebug = function(t, ...)
+	local text = advtrains.print_concat_table({t, ...})
+	minetest.log("action", "[advtrains]"..text)
+	--minetest.chat_send_all("[advtrains]"..text)
+	beerchat.send_on_channel("[advtrains]", "advtrains", text)
+end
