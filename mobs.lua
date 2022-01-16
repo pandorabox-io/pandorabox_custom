fadd98024ea9cc2cf82f41027ed94b2352b5a070
Luke aka SwissalpS <Luke@SwissalpS.ws>
2022-01-16 16:53:22 +0100

    add version check and update to newest version
    also adjusted varname to what author is using
    added mobs to optional dependencies for perfectness sake
    and fixed beowulf typo

--- mobs.lua
+++ mobs.lua
@@ -1,9 +1,10 @@
--- mobs and beowolf don't play nicely together when stone is made
--- dangerous by beowolf, we counteract that here to make mobs not fear
+if 20220116 > tonumber(mobs.version) then return end
+-- mobs and beowulf don't play nicely together when stone is made
+-- dangerous by beowulf, we counteract that here to make mobs not fear
 -- stone and become more challenging again
 local is_node_dangerous = mobs.is_node_dangerous
-function mobs.is_node_dangerous(mob_obj, nodename)
+function mobs:is_node_dangerous(mob_object, nodename)
     if 'default:stone' == nodename then return false end
-    return is_node_dangerous(mob_obj, nodename)
+    return is_node_dangerous(self, mob_object, nodename)
 end
 
