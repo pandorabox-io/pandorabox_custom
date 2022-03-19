72189f224d3ac53a5ed95e86063c764c741d0783
OgelGames <olliverdc28@gmail.com>
2022-03-19 22:20:19 +1100

    remove jungle grass bonemeal deco
    added to bonemeal mod here: https://notabug.org/TenPlus1/bonemeal/commit/4c50a02a0ee8f70aeeb3871a7a7d6bd4582ac34e

--- bonemeal.lua
+++ bonemeal.lua
@@ -1,11 +1,5 @@
 
 -- make more plants obtainable by mulching
--- junglegrass is used to make many nodes, such as moreblocks:clean_glass
-bonemeal:add_deco({
-	{ "default:dirt_with_rainforest_litter",
-		{"default:junglegrass", ""}, {} }
-})
-
 bonemeal:add_deco({
 	{ "default:dirt_with_coniferous_litter",
 		{}, {"default:fern_1", "default:fern_2", "default:fern_3", "", ""} }
