2bab2af4cebfe6c336a1a2fe276bf6c54918247d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-04-25 07:29:52 +0200

    Revert "add login-attempt limiter (currently set to one per 5 seconds)"
    This reverts commit 57a17af78499c7476062c6b1e43a475b60cfd8b0.

--- prejoin.lua
+++ prejoin.lua
@@ -1,18 +1,4 @@
 
--- ip -> os.time()
-local last_prejoin = {}
-
 minetest.register_on_prejoinplayer(function(name, ip)
 	minetest.log("action", "Player " .. name .. " prejoins from " .. ip)
-	local now = os.time()
-	if last_prejoin[ip] then
-		-- there was a login attempt previously from this ip, check the delta-t
-		local dt = now - last_prejoin[ip]
-		if dt < 5 then
-			-- limit prejoin to one attempt per 5 seconds
-			minetest.log("action", "Mass-login detected from '" .. ip .. "'")
-			return "Too many login-attempts, please wait a couple seconds before trying again"
-		end
-	end
-	last_prejoin[ip] = now
 end)
