117dcb6eaa5ccc5aa9cf598b2efd3bd940040653
Dennis Jenkins <dennis.jenkins.75@gmail.com>
2022-09-24 23:48:42 -0700

    Add additional stealthnodes: copperblock, goldblock, blast_resistant_concrete.

--- mesecons_stealthnodes.lua
+++ mesecons_stealthnodes.lua
@@ -1,8 +1,11 @@
 
 local nodes = {
+    {"default", "copperblock"},
+    {"default", "goldblock"},
     {"scifi_nodes", "white"},
-    {"technic", "zinc_block"},
-    {"technic", "chromium_block"}
+    {"technic", "blast_resistant_concrete"},
+    {"technic", "chromium_block"},
+    {"technic", "zinc_block"}
 }
 
 for _,value in pairs(nodes) do
