10a31e9600cbaca9fdd2596303116002b773b7be
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2023-07-08 13:28:49 +0200

    re-add bush aliases

--- alias.lua
+++ alias.lua
@@ -1,2 +1,8 @@
--- intentionally left blank
--- for future mod-aliases
+-- 2019-08-16 bushes removal
+minetest.register_alias("bushes:youngtree2_bottom", "air")
+minetest.register_alias("bushes:bushbranches1", "air")
+minetest.register_alias("bushes:bushbranches2", "air")
+minetest.register_alias("bushes:bushbranches3", "air")
+minetest.register_alias("bushes:bushbranches4", "air")
+minetest.register_alias("bushes:BushLeaves1", "air")
+minetest.register_alias("bushes:BushLeaves2", "air")
