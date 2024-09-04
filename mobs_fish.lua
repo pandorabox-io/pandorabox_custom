f26bd0d45e11130126e6534d8524a9f93a7b5491
Luke aka SwissalpS <161979+SwissalpS@users.noreply.github.com>
2024-09-04 11:16:45 +0200

    Exclude moon and asteroid areas

--- mobs_fish.lua
+++ mobs_fish.lua
@@ -1,9 +1,12 @@
--- add extra areas where fish spawn
+-- Add extra areas where fish spawn
 
 local l_spawn_chance = 10000
 local l_water_level = minetest.settings:get("water_level")
-local l_max_height = 16999 -- or 17999 if we want to include warzone
+local l_max_height_Earth = 777 -- Vacuum sometimes extends well below 1k
+local l_max_height_Mars = 15777
+local l_min_height_Mars = 11111
 
+-- Extend Earth
 mobs:spawn({
 	name = "mobs_fish:clownfish",
 	nodes = {
@@ -14,7 +17,7 @@ mobs:spawn({
 	min_light = 5,
 	interval = 30,
 	chance = l_spawn_chance,
-	max_height = l_max_height,
+	max_height = l_max_height_Earth,
 	min_height = l_water_level,
 	active_object_count = 5
 })
@@ -30,8 +33,40 @@ mobs:spawn({
 	min_light = 5,
 	interval = 30,
 	chance = l_spawn_chance,
-	max_height = l_max_height,
+	max_height = l_max_height_Earth,
 	min_height = l_water_level,
 	active_object_count = 5
 })
 
+
+-- Extend Mars
+mobs:spawn({
+	name = "mobs_fish:clownfish",
+	nodes = {
+		"default:water_source", "default:water_flowing",
+		"default:river_water_source", "default:river_water_flowing"
+	},
+	neighbors =  {"default:sand","default:dirt","group:seaplants","group:seacoral"},
+	min_light = 5,
+	interval = 30,
+	chance = l_spawn_chance,
+	max_height = l_max_height_Mars,
+	min_height = l_min_height_Mars,
+	active_object_count = 5
+})
+
+
+mobs:spawn({
+	name = "mobs_fish:tropical",
+	nodes = {
+		"default:water_source", "default:water_flowing",
+		"default:river_water_source", "default:river_water_flowing"
+	},
+	neighbors =  {"default:sand","default:dirt","group:seaplants","group:seacoral"},
+	min_light = 5,
+	interval = 30,
+	chance = l_spawn_chance,
+	max_height = l_max_height_Mars,
+	min_height = l_min_height_Mars,
+	active_object_count = 5
+})
