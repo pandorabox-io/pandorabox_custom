417719c145dc6da0b16eda46274f9acd75a59bcc
Luke aka SwissalpS <161979+SwissalpS@users.noreply.github.com>
2025-01-01 02:44:17 +0100

    Update ccompass.lua

--- ccompass.lua
+++ ccompass.lua
@@ -3,6 +3,6 @@ ccompass.is_safe_target_under = function(target, nodename)
 	if 'default:stone' == nodename then
 		return true
 	end
-	
+
 	return ccompass_is_safe_target_under(target, nodename)
 end
